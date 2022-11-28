9ff42a75dc39988a79ba1b8d51ae2de7f357a4a3
Всеволод <mixseva@gmail.com>
2022-11-29 01:10:54 +0300

    Initial Commit

---
+++ git/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  git
+//
+//  Created by Всеволод Михайлов on 29.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //
+        //cdsvfd
+        //vdfscvfd
+        //vdfv
+    }
+
+
+}
+
